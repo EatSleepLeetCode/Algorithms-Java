683528da503b2ea4de992c348b643537383b7809
dkang3 <dkang3@ncsu.edu>
2018-01-21 13:59:31 -0600

    PartitionEqualSubsetSum

---
+++ PartitionEqualSubsetSum.java
@@ -0,0 +1,50 @@
+
+public class PartitionEqualSubsetSum 
+{
+    public boolean canPartition(int[] nums)
+    {
+        int n = nums.length;
+        
+        int sum = 0;
+        for(int val : nums)
+            sum += val;
+        
+        if(sum % 2 != 0)
+            return false;
+        
+        sum = sum / 2;
+        
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = true;
+            
+        for(int j = 0; j <= sum; j++)
+            dp[0][j] = false;
+            
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= sum; j++)
+            {
+                if(j - nums[i - 1] >= 0)
+                {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum];        
+    }
+	    
+	public static void main(String[] args) 
+	{
+		PartitionEqualSubsetSum obj = new PartitionEqualSubsetSum();
+		int[] nums = new int[] {1,5,11,5};
+		System.out.println(obj.canPartition(nums));
+	}
+
+}
