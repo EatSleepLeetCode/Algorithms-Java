95c4d9cc9f37fbf219d2dcb1d1dd0bee697c93d6
dkang3 <dkang3@ncsu.edu>
2018-02-10 21:56:26 -0600

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,47 @@
+
+public class Fibonacci 
+{
+	int fibRecursion(int n)
+	{
+		if(n <= 1)
+			return n;
+		
+		return fibRecursion(n - 1) + fibRecursion(n - 2);
+	}
+	
+	int fibIterative(int n)
+	{
+		int first = 0;
+		int second = 1;
+		int third = 0;
+		for(int i = 2; i <= n; i++)
+		{
+			third = second + first;
+			first = second;
+			second = third;
+		}
+		return third;
+	}
+
+	int fibDP(int n)
+	{
+		int[] dp = new int[n + 1]; 
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for(int i = 2; i <= n; i++)
+		{
+			dp[i] = dp[i - 1] + dp [i - 2];
+		}
+		return dp[n];
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		Fibonacci obj = new Fibonacci();
+		System.out.println(obj.fibRecursion(9));
+		System.out.println(obj.fibIterative(9));
+		System.out.println(obj.fibDP(9));
+	}
+}
