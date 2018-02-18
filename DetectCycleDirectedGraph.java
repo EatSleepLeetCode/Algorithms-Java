4114d4def6dd936e43734e42f80c5d0fd71f482d
dkang3 <dkang3@ncsu.edu>
2018-02-18 16:29:03 -0600

    DetectCycleDirectedGraph

--- DetectCycleDirectedGraph.java
+++ DetectCycleDirectedGraph.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class DetectCycleDirectedGraph 
 {
-	boolean detectCycle(Graph graph, Map<Integer, Integer> visited)
+	boolean detectCycle(GraphDirected graph, Map<Integer, Integer> visited)
 	{
 		for(int vertex : graph.vertices)
 		{
@@ -23,20 +23,23 @@ public class DetectCycleDirectedGraph
 		return false;
 	}
 	
-	boolean detectCycleUtil(Graph graph, Map<Integer, Integer> visited, int src)
+	boolean detectCycleUtil(GraphDirected graph, Map<Integer, Integer> visited, int src)
 	{
 		visited.put(src, 1);
 		
-		Set<Integer> neighbors = graph.adjList.get(src); 
+		Set<Integer> neighbors = graph.adj.get(src); 
 		
 		if(neighbors != null)
 		{
 			for(int neighbor : neighbors)
 			{
+				if(visited.get(neighbor) == 2)
+					continue;
+				
 				if(visited.get(neighbor) == 1)
 					return true;
 				
-				if(detectCycleUtil(graph, visited, neighbor))
+				if(detectCycleUtil(graph, visited, neighbor))	//only check if visited state is 0 i.e. in white set
 				{
 					return true;
 				}
@@ -49,14 +52,14 @@ public class DetectCycleDirectedGraph
 	
 	public static void main(String[] args) 
 	{
-/*
-		Graph graph = new Graph();
-		graph.addEdge(1, 2);
-		graph.addEdge(2, 3);
-		graph.addEdge(3, 1);
-*/		
 
-		Graph graph = new Graph();
+//		Graph graph = new Graph();
+//		graph.addEdge(1, 2);
+//		graph.addEdge(2, 3);
+//		graph.addEdge(3, 1);
+		
+
+		GraphDirected graph = new GraphDirected();
 		graph.addEdge(1, 2);
 		graph.addEdge(1, 3);
 		graph.addEdge(2, 3);
