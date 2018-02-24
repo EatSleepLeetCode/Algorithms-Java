d1195406370faea160c526ccfb9574cb9d470cc1
dkang3 <dkang3@ncsu.edu>
2018-02-24 15:03:27 -0600

    BoundaryOfBinaryTree

--- BoundaryOfBinaryTree.java
+++ BoundaryOfBinaryTree.java
@@ -1,45 +1,153 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BoundaryOfBinaryTree 
 {
+	//This recursive solution works for all cases
+	public List<Integer> boundaryOfBinaryTree(TreeNode root)
+	{
+		List<Integer> result = new ArrayList<Integer>();
+		result.add(root.val);
+		getLeftBoundary(root.left, result);
+		getLeafBoundary(root.left, result);
+		getLeafBoundary(root.right, result);
+		getRightBoundary(root.right, result);
+		return result;
+	}
+	
+	void getLeftBoundary(TreeNode node, List<Integer> curr)
+	{
+		if(node == null)
+			return;
+		
+		if(node.left != null)
+		{
+			curr.add(node.val);
+			getLeftBoundary(node.left, curr);
+		}
+		else if(node.right!= null)
+		{
+			curr.add(node.val);
+			getLeftBoundary(node.right, curr);
+		}
+	}
+
+	void getRightBoundary(TreeNode node, List<Integer> curr)
+	{
+		if(node == null)
+			return;
+		
+		if(node.right != null)
+		{
+			getRightBoundary(node.right, curr);
+			curr.add(node.val);
+		}
+		else if(node.left!= null)
+		{
+			getRightBoundary(node.left, curr);
+			curr.add(node.val);
+		}
+	}
+	
+	void getLeafBoundary(TreeNode node, List<Integer> curr)
+	{
+		if(node == null)
+			return;
+		
+		getLeafBoundary(node.left, curr);
+		
+		if(node.left == null && node.right == null)
+		{
+			curr.add(node.val);
+		}
+		
+		getLeafBoundary(node.right, curr);
+	}
+
+	/*
+	//This iterative solution only works for complete or full binary tree
 	public List<Integer> boundaryOfBinaryTree(TreeNode root)
 	{
 		List<Integer> result = new ArrayList<Integer>();
 		if(root == null)
 			return result;
-		result.add(root.val);
 		
-		getElements(result, root.left, true, false);
-		getElements(result, root.right, false, true);
+		List<Integer> left = new ArrayList<Integer>();
+		List<Integer> leaf = new ArrayList<Integer>();
+		List<Integer> right = new ArrayList<Integer>();
+		
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		Queue<Boolean> isLeftNodeQ = new LinkedList<Boolean>();
+		
+		q.offer(root);
+		isLeftNodeQ.offer(true);
+		
+		while(!q.isEmpty())
+		{
+			int size = q.size();		
+			for(int i = 0; i < size; i++)
+			{
+				boolean isLeaf = true;
+				
+				TreeNode curr = q.poll();
+				boolean isLeft= isLeftNodeQ.poll();
+				
+				if(curr.left != null)
+				{
+					q.offer(curr.left);
+					isLeftNodeQ.offer(true);
+					isLeaf = false;
+				}			
+				if(curr.right != null)
+				{
+					q.offer(curr.right);
+					isLeftNodeQ.offer(false);
+					isLeaf = false;
+				}
+	
+				if(isLeaf)
+				{
+					leaf.add(curr.val);
+				}
+				else if(size == 1)
+				{
+					if(isLeft)
+						left.add(curr.val);
+					else
+						right.add(0, curr.val);
+				}
+				else if(size > 1)
+				{
+					if(i == 0)
+					{
+						left.add(curr.val);
+					}
+					else if(i == size - 1)
+					{
+						right.add(0, curr.val);
+					}
+				}
+			}
+		}
+		
+		for(int leftNode : left)
+		{
+			result.add(leftNode);
+		}
+		for(int leafNode : leaf)
+		{
+			result.add(leafNode);
+		}
+		for(int rightNode : right)
+		{
+			result.add(rightNode);
+		}	
 		
 		return result;
 	}
-	
-	void getElements(List<Integer> result, TreeNode root, boolean isLeftBoundary, boolean isRightBoundary)
-	{
-		if(root == null)
-			return;
-		
-		if(root.left == null && root.right == null)
-		{
-			result.add(root.val);
-			return;
-		}
-		
-		if(isLeftBoundary)
-		{
-			result.add(root.val);
-		}
-
-		getElements(result, root.left, root.left != null && isLeftBoundary, root.right == null && isRightBoundary);
-		getElements(result, root.right, root.left == null && isLeftBoundary, root.right != null && isRightBoundary);
-
-		if(isRightBoundary)
-		{
-			result.add(root.val);
-		}
-	}
+	*/
 	
 	public static void main(String[] args)
 	{
@@ -54,6 +162,12 @@ public class BoundaryOfBinaryTree
 		root.right.left = new TreeNode(6);
 		root.right.left.left = new TreeNode(9);
 		root.right.left.right = new TreeNode(10);
+		
+//		BoundaryOfBinaryTree obj = new BoundaryOfBinaryTree();
+//		TreeNode root = new TreeNode(1);
+//		root.right = new TreeNode(2);
+//		root.right.left = new TreeNode(3);
+//		root.right.left.left= new TreeNode(4);
 		
 		System.out.println(obj.boundaryOfBinaryTree(root));
 	}
