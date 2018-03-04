ccfa324743740430e90917fe23ac8950fc8592ca
dkang3 <dkang3@ncsu.edu>
2018-03-04 00:15:17 -0600

    NumSubarraysBoundedMax

--- NumSubarraysBoundedMax.java
+++ NumSubarraysBoundedMax.java
@@ -1,7 +1,8 @@
 
 public class NumSubarraysBoundedMax 
 {
-    public int numSubarrayBoundedMax(int[] A, int L, int R) 
+	 //Solution 1 - O(n^2)
+    public int numSubarrayBoundedMax1(int[] A, int L, int R) 
     {
         int n = A.length;        
         int count = 0;
@@ -25,10 +26,40 @@ public class NumSubarraysBoundedMax
         }
         return count;
     }
+
+    //Solution 2 - O(n)
+    public int numSubarrayBoundedMax2(int[] A, int L, int R) 
+    {
+        int res = 0, heads = 0, tails = 0;
+        for (int val : A) 
+        {
+            if (L <= val && val <= R) 
+            {
+                // val is a head. All tails promoted to heads
+                heads+= tails + 1;
+                tails = 0;
+                res += heads;
+            }
+            else if (val < L) 
+            {
+                // val is a tail, can extend existing subarrays
+                tails++;
+                res += heads;
+            }
+            else 
+            {
+                // combo breaker
+                heads = 0;
+                tails = 0;
+            }
+        }
+        return res;
+    }
     
 	public static void main(String[] args) 
 	{
 		NumSubarraysBoundedMax obj = new NumSubarraysBoundedMax();
-		System.out.println(obj.numSubarrayBoundedMax(new int[] {2,1,4,3}, 2, 3));
+		System.out.println(obj.numSubarrayBoundedMax1(new int[] {2,1,4,3}, 2, 3));
+		System.out.println(obj.numSubarrayBoundedMax2(new int[] {2,1,4,3}, 2, 3));
 	}
 }
