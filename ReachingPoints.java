f2b5c8d9ddc14d4a1616de6e12cf806ce78c0222
dkang3 <dkang3@ncsu.edu>
2018-03-25 23:13:54 -0500

    ReachingPoints

--- ReachingPoints.java
+++ ReachingPoints.java
@@ -4,9 +4,8 @@ import java.util.Set;
 
 public class ReachingPoints 
 {
-	 // Solution 1 - Naive Recursion - TLE    
-    /*
-    public boolean reachingPoints(int sx, int sy, int tx, int ty) 
+	// Solution 1 - Naive Recursion - TLE    
+    public boolean reachingPoints1(int sx, int sy, int tx, int ty) 
     {
         return helper(sx, sy, tx, ty);
     }
@@ -20,54 +19,9 @@ public class ReachingPoints
         
         return helper(x + y, y, X, Y) || helper(x, x + y, X, Y);
     }
-    */
     
-    // Solution 2 - DP - TLE
-    /*
-    boolean found = false;
-	public boolean reachingPoints(int sx, int sy, int tx, int ty) 
-    {
-        Set<Points> seen = new HashSet<Points>();
-        helper(new Points(sx, sy), tx, ty, seen);                
-        return found;        
-    }
-	    
-    void helper(Points p, int tx, int ty, Set<Points> seen)
-    {
-        if(found)
-            return;
-        if(seen.contains(p))
-            return;
-        if(p.x > tx || p.y > ty)
-            return;
-
-        if(p.x == tx && p.y == ty)
-        {
-            found = true;
-            return;
-        }
-
-        seen.add(p);
-        helper(new Points(p.x, p.x + p.y), tx, ty, seen);
-        helper(new Points(p.x + p.y, p.y), tx, ty, seen);
-    }
-
-    class Points
-    {
-        int x;
-        int y;
-
-        public Points(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    */
-    
-    //Solution 3 - Work Backwards - TLE
-    /*
-    public boolean reachingPoints(int sx, int sy, int tx, int ty) 
+    //Solution 2 - Work Backwards - TLE
+    public boolean reachingPoints2(int sx, int sy, int tx, int ty) 
     {
         while (tx >= sx && ty >= sy) 
         {
@@ -81,16 +35,12 @@ public class ReachingPoints
         }
         return false;
     }
-    */
     
-    //Solution 4 - Work Backwards with modulo
-    public boolean reachingPoints(int sx, int sy, int tx, int ty) 
+    //Solution 3 - Work Backwards with modulo
+    public boolean reachingPoints3(int sx, int sy, int tx, int ty) 
     {
         while (tx >= sx && ty >= sy) 
         {
-            if (tx == ty) 
-                break;
-            
             if (tx > ty) 
             {
                 if (ty > sy) 
@@ -108,10 +58,10 @@ public class ReachingPoints
         }
         return (tx == sx && ty == sy);
     }
-	
+    
 	public static void main(String[] args) 
 	{
 		ReachingPoints obj = new ReachingPoints();
-		System.out.println(obj.reachingPoints(1, 7, 3, 5));
+		System.out.println(obj.reachingPoints3(1, 7, 3, 5));
 	}
 }
