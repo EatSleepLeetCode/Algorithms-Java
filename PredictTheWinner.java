5a3140dae916a7002cb8bef065e41fea39824f16
EatSleepLeetCode <dkang3@ncsu.edu>
2018-06-12 23:37:52 -0500

    PredictTheWinner

--- PredictTheWinner.java
+++ PredictTheWinner.java
@@ -1,6 +1,12 @@
 
 public class PredictTheWinner 
 {
+    /*
+    The dp[i][j] saves how much more scores that the first-in-action player will get from i to j 
+    than the second player. First-in-action means whomever moves first. You can still make the 
+    code even shorter but I think it looks clean in this way.
+    */
+
     public boolean PredictTheWinner(int[] nums) 
     {
         int n = nums.length;
@@ -20,5 +26,12 @@ public class PredictTheWinner
             }
         }
         return dp[0][n - 1] >= 0;
+    }
+    
+    public static void main(String[] args)
+    {
+    	PredictTheWinner obj = new PredictTheWinner();
+    	System.out.println(obj.PredictTheWinner(new int[] {1, 5, 2}) == true ? "True" : "False");
+    	System.out.println(obj.PredictTheWinner(new int[] {1, 5, 233, 7}) == true ? "True" : "False");
     }
 }
