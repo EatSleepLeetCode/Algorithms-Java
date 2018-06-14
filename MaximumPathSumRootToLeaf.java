38f1ec868a0bb740d8d8204a3501d64cab1f0e73
EatSleepLeetCode <dkang3@ncsu.edu>
2018-06-13 23:05:27 -0500

    MaximumPathSumRootToLeaf

--- MaximumPathSumRootToLeaf.java
+++ MaximumPathSumRootToLeaf.java
@@ -29,4 +29,17 @@ public class MaximumPathSumRootToLeaf
 	    
 	    return sum + Math.max(leftSum, rightSum);
 	}
+	
+	public static void main(String[] args)
+	{
+		MaximumPathSumRootToLeaf obj = new MaximumPathSumRootToLeaf();
+		TreeNode root = new TreeNode(10);
+		root.left = new TreeNode(6);
+		root.right = new TreeNode(7);
+		root.left.left = new TreeNode(2);
+		root.left.right = new TreeNode(4);
+		root.right.left = new TreeNode(1);
+		
+		System.out.println(obj.maxPathSum(root));
+	}
 }
