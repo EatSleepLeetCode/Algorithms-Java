18f663ead427266adb5928b66ef51485a9de5784
EatSleepLeetCode <dkang3@ncsu.edu>
2018-06-21 00:22:46 -0500

    FindKClosestElements

---
+++ FindKClosestElements.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FindKClosestElements 
+{
+    public List<Integer> findClosestElements(int[] arr, int k, int x) 
+    {
+    	List<Integer> myList = new ArrayList<Integer>();
+    	
+    	for (int num : arr)
+    		myList.add(num);
+        
+        Collections.sort(myList, (a, b) -> Integer.compare(a - x, b - x));
+        
+        myList = myList.subList(0, k);
+        
+        Collections.sort(myList);
+        
+        return myList;
+    }
+    
+	public static void main(String[] args) 
+	{
+		FindKClosestElements obj = new FindKClosestElements();
+		System.out.println(obj.findClosestElements(new int[] {1,2,3,4,5}, 4, 3));
+		System.out.println(obj.findClosestElements(new int[] {1,2,3,4,5}, 4, -1));
+	}
+}
