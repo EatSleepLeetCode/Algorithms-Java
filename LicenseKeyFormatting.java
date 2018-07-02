78c25f0286e5eb5db11f4bc0c0019cebf2e9e2fb
EatSleepLeetCode <33503486+EatSleepLeetCode@users.noreply.github.com>
2018-07-01 23:36:57 -0500

    LicenseKeyFormatting

---
+++ LicenseKeyFormatting.java
@@ -0,0 +1,17 @@
+class LicenseKeyFormatting
+{
+    public String licenseKeyFormatting(String S, int K) 
+    {
+        int n = S.length();
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (S.charAt(i) != '-')
+            {
+                sb.append(sb.length() % (K + 1) == K ? "-" : "").append(S.charAt(i));
+            }
+        }
+        return sb.reverse().toString().toUpperCase();
+    }
+}
