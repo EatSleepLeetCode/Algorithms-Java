a0346a924c33c116c2b9aede01637757c5d61962
EatSleepLeetCode <33503486+EatSleepLeetCode@users.noreply.github.com>
2018-07-04 22:07:24 -0500

    RangeAdditionII

--- RangeAdditionII.java
+++ RangeAdditionII.java
@@ -5,7 +5,7 @@ class RangeAdditionII
         for (int[] op : ops)
         {
             m = Math.min(m, op[0]);
-            n = Math.min(m, op[1]);
+            n = Math.min(n, op[1]);
         }
         return m * n;
     }
