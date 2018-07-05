45177fb54e7a3b1c39f69a26269e6a110be821f2
EatSleepLeetCode <33503486+EatSleepLeetCode@users.noreply.github.com>
2018-07-05 12:08:56 -0500

    NonDecreasingArray

---
+++ NonDecreasingArray.java
@@ -0,0 +1,45 @@
+class NonDecreasingArray 
+{
+    //Solution 1 - Modify input
+    public boolean checkPossibility1(int[] nums) 
+    {
+        int modified = 0;
+        
+        for (int i = 1; i < nums.length; i++)
+        {
+            if (nums[i - 1] > nums[i])
+            {
+                if (modified > 0) return false;
+                modified++;
+                
+                if (i - 2 < 0 || nums[i - 2] <= nums[i])
+                    nums[i - 1] = nums[i];              //lower nums[i - 1]
+                else
+                    nums[i] = nums[i - 1];              //raise nums[i]
+            }
+        }
+        return true;
+    }
+    
+    //Solution - Don't modify input
+    public boolean checkPossibility(int[] nums) 
+    {
+        int modified = 0;
+        int prev = nums[0];
+        
+        for (int i = 1; i < nums.length; i++)
+        {
+            if (prev > nums[i])
+            {
+                if (modified > 0) return false;
+                modified++;
+
+                if (i - 2 >= 0 && nums[i - 2] > nums[i])
+                    continue;
+                
+                prev = nums[i];
+            }
+        }
+        return true;
+    }
+}
