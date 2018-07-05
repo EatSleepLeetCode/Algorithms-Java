e270bb7d595fc94afd2b934b3eee696e82576f5a
EatSleepLeetCode <33503486+EatSleepLeetCode@users.noreply.github.com>
2018-07-04 22:05:44 -0500

    RangeAdditionII

---
+++ RangeAdditionII.java
@@ -0,0 +1,12 @@
+class RangeAdditionII 
+{
+    public int maxCount(int m, int n, int[][] ops) 
+    {
+        for (int[] op : ops)
+        {
+            m = Math.min(m, op[0]);
+            n = Math.min(m, op[1]);
+        }
+        return m * n;
+    }
+}
